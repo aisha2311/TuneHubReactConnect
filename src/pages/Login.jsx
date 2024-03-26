bd766c5059b6d096ae0113a00286c8deda46473e
aisha <aishaascr@gmail.com>
2024-03-26 23:42:54 +0800

    react connection code of tunehub

---
+++ src/pages/Login.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import axios from 'axios';
+
+  const Login = () => {
+    const [email ,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const handleSubmit = async ()=>{
+      const response = await axios.post('http://localhost:8080/validate',{email,password});
+      console.log(response.data);
+    }
+
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+          <label>Email :</label>
+          <input type='email' name ='email'
+          value ={email}
+          onChange={(e) =>setEmail(e.target.value)}
+                  />
+          <br></br>
+          <label>Password :</label>
+          <input type='password' name ='password'
+            value ={email}
+            onChange={(e) =>setPassword(e.target.value)}
+          
+                  />
+          <br></br>
+           <button type="submit">Submit</button>
+        </form>
+
+    </div>
+  );
+};
+export default Login;
